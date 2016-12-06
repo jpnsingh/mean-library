f3bb9b8d1872d59352fdfeb3e5c7fdfb7e668c8a
jpnsingh <jpnsingh86@gmail.com>
2016-12-06 14:59:32 +0530

    Initial Commit;  - Library app using MEAN stack;  - Initial setup for the application;  - Configured npm;  - Configuring bower;  - Setting up express;  - Setting up static files/directories to be served for the app;

---
+++ app.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    var express = require('express'),
+        app = express(),
+        port = 5000;
+
+    app.use(express.static('public'));
+    app.use(express.static('src/views'));
+
+    app.get('/', function (req, res) {
+        res.send('Hello World!');
+    });
+
+    app.get('/books', function (req, res) {
+        res.send('Hello Books!');
+    });
+
+    app.listen(port, function (error) {
+        console.log('Server running on port: ' + port);
+    })
+})();
