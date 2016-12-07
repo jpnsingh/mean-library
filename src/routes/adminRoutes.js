f19c2304cc01d0839cfd49a3520adc7ba27c2f0c
jpnsingh <jpnsingh86@gmail.com>
2016-12-08 01:11:15 +0530

    Working with MongoDB;  - Setting up admin routes to add books to MongoDB;  - Setting up bookRoutes to get books from MongoDB;

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,13 +1,42 @@
 (function () {
     'use strict';
 
-    var adminRouter = require('express').Router();
+    var adminRouter = require('express').Router(),
+        mongodb = require('mongodb').MongoClient;
+
+    var books = [
+        {
+            title: 'Design Patterns',
+            author: 'GOF'
+        },
+        {
+            title: 'Design Principles',
+            author: 'Head First'
+        },
+        {
+            title: 'Thinking in Java',
+            author: 'Bruce Eckel'
+        },
+        {
+            title: 'Thinking in C++',
+            author: 'Bruce Eckel'
+        }
+    ];
 
     module.exports = function (nav) {
         adminRouter
             .route('/addBooks')
             .get(function (request, response) {
-                response.send('Inserting books...');
+                var url = 'mongodb://localhost:27017/libraryApp';
+
+                mongodb.connect(url, function (error, db) {
+                    var collection = db.collection('books');
+
+                    collection.insertMany(books, function (error, results) {
+                        response.send(results);
+                        db.close();
+                    });
+                });
             });
 
         return adminRouter;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,29 +1,27 @@
 (function () {
     'use strict';
 
-    var booksRouter = require('express').Router();
-
-    var books = [
-        {
-            title: 'Design Patterns',
-            author: 'GOF'
-        },
-        {
-            title: 'Design Principles',
-            author: 'Head First'
-        }
-    ];
+    var booksRouter = require('express').Router(),
+        mongodb = require('mongodb').MongoClient,
+        objectId = require('mongodb').ObjectID;
 
     booksRouter
         .route('/')
         .get(function (req, res) {
-            res.render('bookListView', {
-                title: 'Books',
-                nav: [
-                    {title: 'Books', link: '/books'},
-                    {title: 'Authors', link: '/authors'}
-                ],
-                books: books
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (error, db) {
+                var collection = db.collection('books');
+                collection.find({}).toArray(function (error, results) {
+                    res.render('bookListView', {
+                        title: 'Books',
+                        nav: [
+                            {title: 'Books', link: '/books'},
+                            {title: 'Authors', link: '/authors'}
+                        ],
+                        books: results
+                    });
+                });
             });
         });
 
@@ -31,13 +29,21 @@
         .route('/:id')
         .get(function (req, res) {
             var id = req.params.id;
-            res.render('bookView', {
-                title: 'Books',
-                nav: [
-                    {title: 'Books', link: '/books'},
-                    {title: 'Authors', link: '/authors'}
-                ],
-                book: books[id]
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (error, db) {
+                var collection = db.collection('books');
+
+                collection.findOne({_id: new objectId(id)}, function (error, results) {
+                    res.render('bookView', {
+                        title: 'Books',
+                        nav: [
+                            {title: 'Books', link: '/books'},
+                            {title: 'Authors', link: '/authors'}
+                        ],
+                        book: results
+                    });
+                });
             });
         });
 
